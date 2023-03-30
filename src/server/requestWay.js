3ce8adeeb6481d746714988df2a1ff544830c7c4
务民 <1751917619@qq.com>
2023-03-30 10:58:38 +0800

    feat(routes.js):增加一个request请求

--- src/server/requestWay.js
+++ src/server/requestWay.js
@@ -4,7 +4,9 @@ export const get = (url, params) => {
   return new Promise((resolve, reject) => {
     instance
       .get(url, { params })
-      .then((res) => {resolve(res)})
+      .then((res) => {
+        resolve(res);
+      })
       .catch((err) => reject(err));
   });
 };
@@ -13,6 +15,14 @@ export const post = (url, param) => {
   return new Promise((resolve, reject) => {
     instance
       .post(url, param)
+      .then((res) => resolve(res))
+      .catch((err) => reject(err));
+  });
+};
+//封装对应的通用的request请求
+export const request = (url, method, data) => {
+  return new Promise((resolve, reject) => {
+    instance[method](url, data)
       .then((res) => resolve(res))
       .catch((err) => reject(err));
   });

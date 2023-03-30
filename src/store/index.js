c79d146e77404fc0e3243a6f357adaa3c42607ca
务民 <1751917619@qq.com>
2023-03-30 22:36:31 +0800

    feat(store):新增user的actions和mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
-import Vue from "vue";
+//仓库
 import Vuex from "vuex";
+import Vue from "vue";
+//引入小仓库
 import user from "./modules/user";
 import goods from "./modules/goods";
+//安装插件
 Vue.use(Vuex);
+//对外暴露仓库
+//第一个注意:需要关键字new，你没有new会报错的
+//第二个注意:Store构造函数,书写的时候别小写
 export default new Vuex.Store({
   //模块化store仓库
-  modules: {user,goods},
+  modules: { user, goods },
 });
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,24 +1,57 @@
+import { reqUserInfo } from "@/server/apis.js";
 // state
 const state = {
   name: "我是user模块",
-  TOKEN: "0000",
+  //验证码
+  code: "",
+  //身份标识符很重要【存储在vuex】
+  TOKEN: localStorage.getItem("TOKEN"),
+  //用户名
+  nickName: "",
 };
 // mutations
 const mutations = {
   myMutation() {
     console.log("user模块的 myMutation方法被触发了");
   },
-};
-// getters
-const getters = {
-  reverseName(state) {
-    return state.name.split("").reverse().join("");
+  GET_CODE(state, code) {
+    state.code = code;
+  },
+  SET_TOKEN(state, token) {
+    state.token = token;
+  },
+  SET_USERINFO(state, nickName) {
+    state.nickName = nickName;
+  },
+  CLEAR(state) {
+    //清除仓库相关用户信息
+    state.token = "";
+    state.nickName = "";
+    //本地存储令牌清空
+    localStorage.removeItem("TOKEN");
   },
 };
 // actions
 const actions = {
   myAction() {
     console.log("user模块 action的 myAction方法被触发了");
+  },
+  //获取用户信息
+  async getUserInfo({ commit }) {
+    console.log(111);
+    let result = await reqUserInfo();
+    if (result.code == 200) {
+      commit("SET_USERINFO", result.data.nickName);
+      return "ok";
+    } else {
+      return Promise.reject();
+    }
+  },
+};
+// getters
+const getters = {
+  reverseName(state) {
+    return state.name.split("").reverse().join("");
   },
 };
 
